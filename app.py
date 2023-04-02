f36242ceec8db6c3bbadf8730527891d70fbd53d
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 16:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with pages:
     elif pages.current == 1:
         used_1 = survey.radio(
             "select_from_following",
-           options=f_i[pages.current].split(','),
+           options=f_i[2].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -30,7 +30,7 @@ with pages:
     elif pages.current == 2:
         used_2 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[2].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -38,7 +38,7 @@ with pages:
     elif pages.current == 3:
         used_3 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[3].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -47,7 +47,7 @@ with pages:
     elif pages.current == 4:
         used_4 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[4].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -56,7 +56,7 @@ with pages:
     elif pages.current == 5:
         used_5 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[5].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -64,7 +64,7 @@ with pages:
     elif pages.current == 6:
         used_6 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[6].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -72,7 +72,7 @@ with pages:
     elif pages.current == 7:
         used_7 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[7].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -80,7 +80,7 @@ with pages:
     elif pages.current == 8:
         used_8 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[8].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
@@ -88,7 +88,7 @@ with pages:
     elif pages.current == 9:
         used_9 = survey.radio(
             "select_from_following",
-            options=f_i[pages.current].split(','),
+            options=f_i[9].split(','),
             index=0,
             label_visibility="collapsed",
             horizontal=True,
