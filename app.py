4896748ee56511cfab88cc7a292bd7d5b12ddd83
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-03 03:15:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ def download(json):
          csv2 = csv1.split(',') 
          csv3 = list(filter(lambda a: a != "", csv2))
          csv4 = list(filter(lambda a: a != " ", csv3))
-         csv5 = ','.join(csv4.remove(''))
-         st.write(csv5)
+         csv5 = ','.join(csv4)
+         st.write(csv4)
          st.download_button(
                              label="Download selection as CSV",
                              data=csv5,
