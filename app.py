b5a34360b85919b3494df276c2a78723781c9324
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 17:15:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,17 @@ import streamlit as st
 import streamlit_survey as ss
 import pandas as pd
 
+def download(json):
+         df = pd.read_json(json)
+         df.to_csv('output.csv', encoding='utf-8', index=False)
+         st.download_button(
+                             label="Download selection as CSV",
+                             data=csv,
+                             file_name='output.csv',
+                             mime='text/csv',
+                            )
+         
+    
 
 #radio=[]
 
@@ -10,7 +21,7 @@ f = open('manufacturers.csv', 'r')
 f_i = (f.read()).split('\n')
 
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
-pages = survey.pages(len(f_i), on_submit=lambda: st.write((pd.read_json(survey.to_json())).to_csv()))
+pages = survey.pages(len(f_i), on_submit=lambda: download(survey.to_json()))
 with pages:
          survey.radio(
             "select_from_following",
