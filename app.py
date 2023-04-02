f4c9ca28a40a40a412540ea67a80d93cebb7d91c
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 15:30:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import copy
 
 num = 0
+radio=[]
 
 f = open('manufacturers.csv', 'r')
 
@@ -15,9 +16,12 @@ Selection=['placer']
 
 with st.form('survey',clear_on_submit=True):
      for x in f_i:
-          radio = st.radio('select from following',(x.split(',')))
+          radio.append(st.radio('select from following',(x.split(','))))
       
      submit = st.form_submit_button('submit')
+     if submit:
+          st.write(radio)
+          
  
      
 
