8dbc68bd6268863c8f66b73d496133d76545d4d9
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-03 03:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ def download(json):
          csv1.pop(1)
          csv1.pop(2)
          csv2=[csv1[0][1:],csv1[1].replace('value,','')]
-         st.write(csv2)
+         CSV='/n'.join(csv2)
+         st.write(CSV)
          st.download_button(
                              label="Download selection as CSV",
-                             data=csv,
+                             data=CSV,
                              file_name='output.csv',
                              mime='text/csv',
                             )
