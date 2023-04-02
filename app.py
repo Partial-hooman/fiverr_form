c8071a964c438ab462e37e4c6f27f91a0db6e8a7
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 12:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ f_i = (f.read()).split('\n')
 
 
 survey = ss.StreamlitSurvey()
-For x in range(len(f_i)):
+for x in range(len(f_i)):
     survey.radio('select from the following',options=f_i[x].split(','),index=0,horizontal=True,label_visibility="collapsed")
      
         
