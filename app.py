23c5294986f8733f61b223ff82fb90dfd068ff21
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 15:55:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import streamlit_survey as ss
 
 num = 0
 #radio=[]
@@ -8,16 +8,48 @@ f = open('manufacturers.csv', 'r')
 
 f_i = (f.read()).split('\n')
 
-form = st.form('survey',clear_on_submit=True)
-
-
-
-with form:
-     st.radio('select from following',(f_i[num].split(',')))
-      
-     submit = st.form_submit_button('submit')
-
-          
+survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
+pages = survey.pages(2, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
+with pages:
+    if pages.current == 0:
+        st.write("Have you used Streamlit before?")
+        used_before = survey.radio(
+            "used_st_before",
+            options=["NA", "Yes", "No"],
+            index=0,
+            label_visibility="collapsed",
+            horizontal=True,
+        )
+        if used_before == "Yes":
+            st.write("How often do you use Streamlit?")
+            survey.select_slider(
+                "st_frequency",
+                options=["Every Day", "Every week", "Every Month", "Once a year", "Rarely"],
+                label_visibility="collapsed",
+            )
+        elif used_before == "No":
+            st.write("Have you used other dashboarding tools?")
+            used_other = survey.radio(
+                "used_other",
+                options=["NA", "Yes", "No"],
+                index=0,
+                label_visibility="collapsed",
+                horizontal=True,
+            )
+            if used_other == "Yes":
+                st.write("Which tools?")
+                survey.multiselect(
+                    "other_tools",
+                    options=["Dash", "Voila", "Panel", "Bokeh", "Plotly", "Other"],
+                    label_visibility="collapsed",
+                )
+    elif pages.current == 1:
+        st.write("How satisfied are you with this survey?")
+        survey.select_slider(
+            "Overall Satisfaction",
+            options=["Very Unsatisfied", "Unsatisfied", "Neutral", "Satisfied", "Very Satisfied"],
+            label_visibility="collapsed",
+        )
  
      
 
