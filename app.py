8e427bdee1c15846cba1c2e6808af4c45d5d66a6
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 14:25:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ Selection=['placer']
 
 
 survey = ss.StreamlitSurvey()
-pages = survey.pages(len(f_i), on_submit=lambda: st.write(pages.labels))
+pages = survey.pages(len(f_i), on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
 
 with pages:
      radio = survey.radio('select from the following',options=f_i[pages.current].split(','),index=0,horizontal=True,label_visibility="collapsed")
