59dd38ffec3654820c5f0259fa364e06e7b84f85
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-03 04:10:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
+#import the required libraries 
+
 import streamlit as st
 import streamlit_survey as ss
 import pandas as pd
 
+#the download function to download the csv output, which first takes output as a json file
+
 def download(json):
-         df = pd.read_json(json)
-         csv = df.to_csv()
-         csv1 = csv.split('\n') 
+         df = pd.read_json(json) 
+         csv = df.to_csv() #the json is converted into csv
+         csv1 = csv.split('\n') #the CSV is converted to an array to remove the unecessary rows and columns
+         # removing unecessary rows and columns 
          csv1.pop(1)
          csv1.pop(2)
          csv2=[csv1[0][1:],csv1[1].replace('value,','')]
          CSV='\n'.join(csv2)
-         st.write(CSV)
+         #st.write(CSV)
+         # the CSV output is then exported and then downloaded through streamlit's download button widget 
          st.download_button(
                              label="Download selection as CSV",
                              data=CSV,
@@ -19,15 +25,21 @@ def download(json):
                             )
          
     
-
-#radio=[]
+# the manufacturer CSV file is imported
 
 f = open('manufacturers.csv', 'r')
 
+# it's then converted to an array to generate the survey 
+
 f_i = (f.read()).split('\n')
 
+#generating the survey
+
+
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
-pages = survey.pages(len(f_i), on_submit=lambda: download(survey.to_json()))
+pages = survey.pages(len(f_i), on_submit=lambda: download(survey.to_json())) # the survey is first converted to json after which it is given to the download function to download the CSV output
+# generating the survey radios
+
 with pages:
          survey.radio(
             options=f_i[pages.current].split(','),
