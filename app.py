5f15b77033db70541b69cbca0214645746906638
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 10:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit
 
+survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
+pages = survey.pages(2, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
+with pages:
+     if pages.current == 0:
+        st.write("Have you used Streamlit before?")
+        used_before = survey.radio(
+                                    "used_st_before",
+                                     options=["CHEVROLET","DODGE","FERRARI","HONDA"],
+                                     index=0,
+                                     label_visibility="collapsed",
+                                     horizontal=True,
+                                     )
