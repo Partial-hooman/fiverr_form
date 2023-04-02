81b5e65398df8924cd512438136e1964d70b31ac
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 11:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,16 +15,9 @@ f_i = (f.read()).split('\n')
 
 
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
-pages = survey.pages(len(f_i) - 1, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
+pages = survey.pages(len(f_i), on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
 with pages:
-     if pages.current == 0:
-        st.write("Have you used Streamlit before?")
-        for x in f_i:
-            #st.write(f_i[0]) 
-            survey.radio(
-                         "used_st_before",
-                          options=x.split(','),
-                          index=0,
-                          label_visibility="collapsed",
-                          horizontal=True,
-                          )
+     survey.radio(options=f_i[pages.current].split('\n'),index=pages.current,horizontal=True)
+        
+        
+            
