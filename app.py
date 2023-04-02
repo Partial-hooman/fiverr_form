25462ed0722db099024e2b8d5cea9e07c5374267
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 13:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ f = open('manufacturers.csv', 'r')
 
 f_i = (f.read()).split('\n')
 
-
+Selection=[]
 
 
 
@@ -18,8 +18,8 @@ survey = ss.StreamlitSurvey()
 pages = survey.pages(len(f_i), on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
 
 with pages:
-     survey.radio('select from the following',options=f_i[pages.current].split(','),index=0,horizontal=True,label_visibility="collapsed")
-    
+     radio = survey.radio('select from the following',options=f_i[pages.current].split(','),index=0,horizontal=True,label_visibility="collapsed")
+     st.write(radio)
      
         
             
