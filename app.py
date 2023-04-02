9a615fb4769d2d7ccfec07c21d1660348e4b16e4
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 16:55:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import streamlit_survey as ss
+import pandas as pd
 
-num = 0
+
 #radio=[]
 
 f = open('manufacturers.csv', 'r')
@@ -9,7 +10,7 @@ f = open('manufacturers.csv', 'r')
 f_i = (f.read()).split('\n')
 
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
-pages = survey.pages(len(f_i), on_submit=lambda: st.json(survey.to_json()))
+pages = survey.pages(len(f_i), on_submit=lambda: st.write((pd.read_json(survey.to_json())).to_csv())
 with pages:
          survey.radio(
             "select_from_following",
