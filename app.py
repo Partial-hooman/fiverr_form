4697079f22cd0f693747d24ea380960ab68f3348
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 13:26:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ pages = survey.pages(len(f_i), on_submit=lambda: st.success("Your responses have
 
 with pages:
      survey.radio('select from the following',options=f_i[pages.current].split(','),index=0,horizontal=True,label_visibility="collapsed")
-
+     if pages.current == len(f_i)-1:
+              st.json(survey.to_json())
      
         
             
