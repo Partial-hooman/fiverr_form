c947c2cba6e391f0600db72b3529d1d88b275f49
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 11:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import streamlit_survey as ss
 import streamlit as st
 
+f = open('manufacturers.csv', 'r')
+
+f_i = (f.read()).split('\n')
+
+
+
+
+
+
+
+
+
 
 survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
 pages = survey.pages(2, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
 with pages:
      if pages.current == 0:
         st.write("Have you used Streamlit before?")
-        used_before = survey.radio(
-                                    "used_st_before",
-                                     options=["CHEVROLET","DODGE","FERRARI","HONDA"],
+        for x in f_i:
+               
+             used_before = survey.radio(
+                                   "used_st_before",
+                                     options=x,
                                      index=0,
                                      label_visibility="collapsed",
-                                     horizontal=True,
+                                    horizontal=True,
                                      )
