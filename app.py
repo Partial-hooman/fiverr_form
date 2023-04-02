2b25dd75f1bf8d35585872d1f757c2f71f069038
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-02 12:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ f_i = (f.read()).split('\n')
 survey = ss.StreamlitSurvey()
 pages = survey.pages(1, on_submit=lambda: st.success("Your responses have been recorded. Thank you!"))
 with pages:
-     For x in range(len(f_i)-1):
+     For x in range(len(f_i)):
          survey.radio('select from the following',options=f_i[x].split(','),index=0,horizontal=True,label_visibility="collapsed")
      
         
