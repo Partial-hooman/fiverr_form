fe8b4cb06e9544fb150056312129107205fb21a7
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-05 02:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def calculate_and_plot_user_preference(input,S,G,O,T):
    P_df = pd.DataFrame(dict(r=[S,G,O,T],theta=["S","G","O","T"]))
    fig = px.line_polar(P_df, r='r', theta='theta', line_close=True)
    st.write(fig)
-
+   st.write(S,G,O,T)
 
 
     
