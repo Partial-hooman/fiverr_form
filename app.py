ef7dfbda73f42008cf4524c02bd10d1790c21812
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-06 09:30:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def input_selection(json):
  
 def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Export results</a>'
 
 
 
