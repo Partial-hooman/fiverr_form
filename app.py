782246ce257b1d61cf186e36005f84f7d466a786
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-09 05:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def calculate_and_plot_user_preference(Input,Lookup,input,S_count,G_count,O_coun
             if i in A:
                st.write(input[i],true)
             else:
-                st.write("not true"
+                st.write("not true")
          if input[i] in inp2.get(S[i]):
             S_count += 1
             st.write("S",S[i])
