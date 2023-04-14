9d9f1140560999458c29f5d3b6c70dcd81ae5d87
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-14 14:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def input_ratings(json,INP):
     st.subheader('Ratings:')    
     st.dataframe(Data)
     ratings = [sum(a_ratings),sum(b_ratings),sum(c_ratings),sum(d_ratings)]
-    return ratings
+    Frame = pd.DataFrame(Data)
+    return ratings, Frame 
 
 
  
@@ -101,29 +102,29 @@ def calculate_and_plot_user_preference(Input,Lookup,input,S_count,G_count,O_coun
             #inp.append("D")
                        
   
-
-   inp2 = {(list(Input.columns))[0]:A,(list(Input.columns))[1]:B,(list(Input.columns))[2]:C,(list(Input.columns))[3]:D}
+   ratings, Frame = input
+   #inp2 = {(list(Input.columns))[0]:A,(list(Input.columns))[1]:B,(list(Input.columns))[2]:C,(list(Input.columns))[3]:D}
    #st.write('['+",".join(inp)+']')
-   for i in range(len(input)):
-         if input[i].strip() in inp2.get(S[i]):
-            S_count += 1
+   #for i in range(len(input)):
+         #if input[i].strip() in inp2.get(S[i]):
+            #S_count += 1
             #st.write("S",S[i])
-         elif input[i].strip() in inp2.get(G[i]):
-            G_count += 1
+         #elif input[i].strip() in inp2.get(G[i]):
+            #G_count += 1
             #st.write("G",G[i])
-         elif input[i].strip() in inp2.get(O[i]):
-            O_count += 1
+         #elif input[i].strip() in inp2.get(O[i]):
+            #O_count += 1
             #st.write("O",O[i])
-         elif input[i].strip() in inp2.get(T[i]):
-            T_count += 1
+         #elif input[i].strip() in inp2.get(T[i]):
+            #T_count += 1
             #st.write("T",T[i])
          
 
-   prefs = {(list(Lookup.columns))[0]:S_count,(list(Lookup.columns))[1]:G_count,(list(Lookup.columns))[2]:O_count,(list(Lookup.columns))[3]:T_count}
-   pref = [S_count,G_count,O_count,T_count]
+   prefs = {(list(Input.columns))[0]:ratings[0],(list(Input.columns))[1]:ratings[1],(list(Input.columns))[2]:ratings[2],(list(Input.columns))[3]:ratings[3]}
+   pref = ratings 
    pref2 = [*set(pref)]
    pref2.sort(reverse = True)
-   st.write('USER RESPONSE: ' + "[" + ','.join(input) + ']')
+   #st.write('USER RESPONSE: ' + "[" + ','.join(input) + ']')
    st.header("User preference is:")
    for x in range(len(pref2)):
        my_srs = pd.Series(prefs).astype(int)
@@ -136,10 +137,10 @@ def calculate_and_plot_user_preference(Input,Lookup,input,S_count,G_count,O_coun
    #st.header("User preference is " + user_pref)
    st.write(prefs)
    #plotting the graph 
-   P_df = pd.DataFrame(dict(r=[S_count,G_count,O_count,T_count],theta=[list(Lookup.columns)[0],list(Lookup.columns)[1],list(Lookup.columns)[2],list(Lookup.columns)[3]]))
+   P_df = pd.DataFrame(dict(r=ratings,theta=[list(Input.columns)[0],list(Input.columns)[1],list(Input.columns)[2],list(Input.columns)[3]]))
    fig = px.line_polar(P_df, r='r', theta='theta', line_close=True,color_discrete_sequence=["#0068c9","#83c9ff","#ff2b2b","#ffabab","#29b09d","#7defa1","#ff8700","#ffd16a","#6d3fc0","#d5dae5"])
    st.plotly_chart(fig)
-   fig2 = px.pie(values=pref, names=[list(Lookup.columns)[0],list(Lookup.columns)[1],list(Lookup.columns)[2],list(Lookup.columns)[3]], hole=.5,color_discrete_sequence=["#0068c9","#83c9ff","#ff2b2b","#ffabab","#29b09d","#7defa1","#ff8700","#ffd16a","#6d3fc0","#d5dae5"])
+   fig2 = px.pie(values=pref, names=[list(Input.columns)[0],list(Input.columns)[1],list(Input.columns)[2],list(Input.columns)[3]], hole=.5,color_discrete_sequence=["#0068c9","#83c9ff","#ff2b2b","#ffabab","#29b09d","#7defa1","#ff8700","#ffd16a","#6d3fc0","#d5dae5"])
    st.plotly_chart(fig2)
    #buffer = io.BytesIO()
    #buffer2 = io.BytesIO()
@@ -192,14 +193,39 @@ def calculate_and_plot_user_preference(Input,Lookup,input,S_count,G_count,O_coun
          row_fill_color=("#ffffff", "#d7d8d6"),
          fig_size=(560, 1440)
         )
+   Df_fig3 = df2img.plot_dataframe(
+         Frame,
+         title=dict(
+              font_color="darkred",
+              font_family="Times New Roman",
+              font_size=16,
+              text="Lookup data:",
+              ),
+         tbl_header=dict(
+              align="right",
+              fill_color="blue",
+              font_color="white",
+              font_size=10,
+              line_color="darkslategray",
+              ),
+         tbl_cells=dict(
+         align="right",
+         line_color="darkslategray",
+              ),
+         row_fill_color=("#ffffff", "#d7d8d6"),
+         fig_size=(560, 1440)
+        )
    df_temp = tempfile.NamedTemporaryFile(suffix='.png')
    df_temp2 = tempfile.NamedTemporaryFile(suffix='.png')
+   df_temp3 = tempfile.NamedTemporaryFile(suffix='.png')
    df2img.save_dataframe(fig=Df_fig, filename=df_temp.name)
    df2img.save_dataframe(fig=Df_fig2, filename=df_temp2.name)
+   df2img.save_dataframe(fig=Df_fig3, filename=df_temp3.name)
    pdf = FPDF()
    pdf.add_page()
    pdf.image(str(df_temp.name))
    pdf.image(str(df_temp2.name))
+   pdf.image(str(df_temp3.name))
    pdf.set_font('Arial', 'B', 16)
    pdf.image(str(temp.name),x=-25)
    pdf.cell(40, 10, 'User preference (radar chart)',align = 'L')
@@ -242,7 +268,7 @@ if Input is not None:
 
       #generating the survey
       survey = ss.StreamlitSurvey("Survey Example - Advanced Usage")
-      pages = survey.pages(len(input.index), on_submit=lambda: input_ratings(survey.to_json(),input)) #calculate_and_plot_user_preference(input,lookup,input_selection(survey.to_json()),S_count,G_count,O_count,T_count,S,G,O,T,A,B,C,D)) # the survey is first converted to json after which it is given to the download function to download the CSV output
+      pages = survey.pages(len(input.index), on_submit=lambda: calculate_and_plot_user_preference(input,lookup,input_ratings(survey.to_json(),input)),S_count,G_count,O_count,T_count,S,G,O,T,A,B,C,D)) # the survey is first converted to json after which it is given to the download function to download the CSV output
 
       # generating the survey radios
 
