5759b2a08fc6ec255d153a190a5a1c3aa210de94
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-15 16:17:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ if Input is not None:
       # generating the survey radios
 
       with pages:
-               col0, col1, col2, col3 = st.columns([1,1,1,1])
+               col0, col1, col2, col3 = st.columns([1,2,3,4])
                with col0:
                        st.write((input.iloc[pages.current].tolist())[0])
                        Slb0 = survey.selectbox("rating:", options=[1,2,3,4],id=f"A_{pages.current}",label_visibility="collapsed")
