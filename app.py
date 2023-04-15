d5dce118a1436bc83e2e40d027e93de765ad5b98
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-15 16:39:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ if Input is not None:
                        st.write("    ")
                        Slb0 = survey.selectbox("rating:", options=[1,2,3,4],id=f"A_{pages.current}",label_visibility="collapsed")
                with padding0:
-                       st.write((input.iloc[pages.current].tolist())[0])
+                       st.write("            " + (input.iloc[pages.current].tolist())[0])
                with col1:
                        st.write((input.iloc[pages.current].tolist())[1])
                        Slb1 = survey.selectbox("rating:", options=[1,2,3,4],id=f"B_{pages.current}",label_visibility="collapsed")
