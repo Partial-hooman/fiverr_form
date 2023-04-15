e0d4e1f336b617dda944aa2d929a9280e5e55fb4
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-15 16:51:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ if Input is not None:
       # generating the survey radios
 
       with pages:
-               col0, padding0, col1, padding1, col2, padding2, col3, padding3 = st.columns([1,1.8,1,2,1,2,1,2])
+               col0, padding0, col1, padding1, col2, padding2, col3, padding3 = st.columns([1,1.69,1,2,1,2,1,2])
                with col0:
                        st.write("    ")
                        st.write("    ")
