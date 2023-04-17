93e445c0ce78893dc6d563d2b4a54b7696095779
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-17 17:59:36 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+    page_title="car comparison",
+    page_icon="",
 )
+
+
+st.write("# CAR COMPARISON")
+
+st.sidebar.success("Select a method.")
