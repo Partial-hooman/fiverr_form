fdeb5304a00cb6ed095d9b498c2423abbe02932e
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-22 18:01:53 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import df2img
+import base64
+import tempfile 
+from fpdf import FPDF
+import plotly.express as px
+import streamlit as st
+import streamlit_survey as ss
+import pandas as pd
 
 st.set_page_config(
     page_title="car comparison",
