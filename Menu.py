7be6a717ffd37220a1efa9bca80531bdc48f3551
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-23 11:23:46 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -186,15 +186,15 @@ with st.sidebar:
     
 
 if choose == "method1":
-  for key in st.session_state.keys():
-       del st.session_state[key]
+  
   Input = st.file_uploader("upload the input csv", type='csv', key="method1")
   Lookup = st.file_uploader("upload the lookup CSV", type='csv',key="method1_lookup")
 
 
   if Input is not None:
     if Lookup is not None:
-
+      for key in st.session_state.keys():
+       del st.session_state[key]
       input = pd.read_csv(Input,sep=r'\s*,\s*',engine='python')
       A = (input.iloc[:,0]).tolist()
       B = (input.iloc[:,1]).tolist()
@@ -407,8 +407,7 @@ def calculate_and_plot_user_preference_m2(Input,input):
                         
                   
 if  choose == "method2":
-   for key in st.session_state.keys():
-       del st.session_state[key]
+   
    _Input = st.file_uploader("upload the input csv", type='csv',key="method2")
 
 
